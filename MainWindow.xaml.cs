9e195f4fc518f73b9029f3d44b8ab7060a8f7988
KHK <iamring0811@naver.com>
2021-12-24 05:05:05 +0900

    1st commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UserProperty User;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void User_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("31:" + e.PropertyName);
+            switch (e.PropertyName)
+            {
+                case "Name":
+                    txtName.Text = User.Name;
+                    break;
+                case "Age":
+                    txtAge.Text = User.Age.ToString();
+                    break;
+            }
+        }
+        private void ViewData(UserProperty User)
+        {
+            txtName.Text = User.Name;
+            txtAge.Text = User.Age.ToString();
+        }
+        private UserProperty GetData()
+        {
+            UserProperty userProperty = new UserProperty("김형관", 34);
+            return userProperty;
+        }
+        private UserProperty GetData2()
+        {
+            UserProperty userProperty = new UserProperty("김로사", 43);
+            return userProperty;
+        }
+
+        private void btnOption_Click(object sender, RoutedEventArgs e)
+        {
+            User = GetData();
+            ViewData(User);
+            User.PropertyChanged += User_PropertyChanged;
+        }
+
+        private void btnOption2_Click(object sender, RoutedEventArgs e)
+        {
+            User = GetData2();
+            ViewData(User);
+            User.PropertyChanged += User_PropertyChanged;
+        }
+
+        private void btnBirthday_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(txtAge.Text) != 0)
+            {
+                DateTime dt = DateTime.Now;
+                int year = dt.Year - Convert.ToInt32(txtAge.Text) + 1;
+                txtTime.Text = year.ToString();
+            }
+                
+        }
+
+        private void btnPlusAge_Click(object sender, RoutedEventArgs e)
+        {
+            User.Age++;
+        }
+    }
+}
---
+++ UserProperty.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class UserProperty: System.ComponentModel.INotifyPropertyChanged//System.ComponentModel.INotifyPropertyChanged : 바인딩 = 데이터 실시간 적용
+    {
+        //바인딩 시작
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void Notify(string propertyName)
+        {
+            if(this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        //바인딩 끝
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name == value) { return; }
+                name = value;
+                Notify("Name");//바인딩 대상
+            }
+        }
+        private int age;
+
+
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (age == value) { return; }
+                age = value;
+                Notify("Age");//바인딩 대상
+            }
+        }
+        public UserProperty() { }
+        public UserProperty(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
